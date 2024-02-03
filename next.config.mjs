1471560e8136f99d55884aadca7cd459f8321cbd
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-03 15:26:07 +0330

    added line of comment to nextJS config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// Codes by mahdi tasha
+// Importing type of nextJS configs
 /** @type {import('next').NextConfig} */
+
+// Defining configs of nextJS
 const nextConfig = {};
 
+// Exporting the configs
 export default nextConfig;
