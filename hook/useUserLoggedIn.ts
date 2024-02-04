e93e3edf38d3f138be720c2631af5a787728c0f8
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-04 16:55:50 +0330

    deleted custom hook and installed zustand

--- hook/useUserLoggedIn.ts
+++
@@ -1,6 +0,0 @@
-// Codes by mahdi tasha
-// Creating a custom hook which checks if user is logged in and exporting it as default
- export default function useUserLoggedIn():boolean {
-    // TODO: This will change when working with backend
-    return false;
-}
