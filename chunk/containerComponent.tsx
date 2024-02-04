af9aa5495a0bfd3815d7e669473dde62e4da7bad
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-04 15:56:41 +0330

    added scroll effect to header

--- chunk/containerComponent.tsx
+++ chunk/containerComponent.tsx
@@ -6,13 +6,15 @@ import {ReactNode} from "react";
 interface propsType {
     children: ReactNode;
     className?: string;
-};
+    noPaddingY?: boolean;
+    noPaddingX?: boolean;
+}
 
 // Creating and exporting container component as default
-export default function ContainerComponent({children, className}:propsType):ReactNode {
+export default function ContainerComponent({children, className, noPaddingY = false, noPaddingX = false}:propsType):ReactNode {
     // Returning JSX
     return (
-        <div className={`max-w-[1140px] mx-auto p-[20px]${(className) ? ` ${className}` : false}`}>
+        <div className={`max-w-[1140px] mx-auto ${(noPaddingX) ? 'px-0' : 'px-[20px]'} ${(noPaddingY) ? 'py-0' : 'py-[20px]'} ${(className) ? ` ${className}` : false}`}>
             {children}
         </div>
     );
--- component/headerComponent.tsx
+++ component/headerComponent.tsx
@@ -1,19 +1,31 @@
+// Forcing nextJS to render this component as client side component
+'use client';
+
 // Importing part
 import {ReactNode} from "react";
 import Image from "next/image";
 import lotusImg from '@/public/img/logo/img-logo-lotus.png';
 import ButtonComponent from "@/chunk/buttonComponent";
 import ContainerComponent from "@/chunk/containerComponent";
+import useScrolled from "@/hook/useScrolled";
 
 // Defining type of props
 interface propsType { isUserLoggedIn: boolean; }
 
 // Creating and exporting header component as default
 export default function HeaderComponent({isUserLoggedIn}:propsType):ReactNode {
+    // Defining custom hook to use later
+    const isScrolled:boolean = useScrolled({
+        onMQ: true
+    });
+
     // Returning JSX
     return (
-        <header className={'lg:absolute top-0 left-0 w-full'}>
-            <ContainerComponent className={
+        <header
+            data-scrolled={isScrolled}
+            className={'lg:fixed top-0 left-0 w-full z-10 transition-all duration-500 lg:data-[scrolled="true"]:bg-white lg:data-[scrolled="true"]:shadow-2xl lg:data-[scrolled="false"]:bg-transparent lg:data-[scrolled="false"]:shadow-none'}
+        >
+            <ContainerComponent noPaddingY className={
                 (isUserLoggedIn)
                     ? 'flex items-center justify-center'
                     : 'flex items-center lg:justify-between justify-center gap-[20px]'
