99b18c9e4ec00717472c386e583c8c7006920ba1
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-04 16:45:08 +0330

    added fourth section of not logged in home page

--- component/innerPage/notLoggedIn/homeNotLoggedIn.tsx
+++ component/innerPage/notLoggedIn/homeNotLoggedIn.tsx
@@ -4,6 +4,7 @@ import {ReactNode} from "react";
 import FirstSectionComponent from '@/component/innerPage/notLoggedIn/section/firstSectionComponent';
 import SecondSectionComponent from "@/component/innerPage/notLoggedIn/section/secondSectionComponent";
 import ThirdSectionComponent from "@/component/innerPage/notLoggedIn/section/thirdSectionComponent";
+import FourthSectionComponent from "@/component/innerPage/notLoggedIn/section/fourthSectionComponent";
 
 // Creating and exporting not logged in home page as default
 export default function HomeNotLoggedIn():ReactNode {
@@ -13,6 +14,7 @@ export default function HomeNotLoggedIn():ReactNode {
             <FirstSectionComponent />
             <SecondSectionComponent />
             <ThirdSectionComponent />
+            <FourthSectionComponent />
         </>
     );
 }
---
+++ component/innerPage/notLoggedIn/section/fourthSectionComponent.tsx
@@ -0,0 +1,37 @@
+// Codes by mahdi tasha
+// Importing part
+import {ReactNode} from "react";
+import mainImage from '@/public/img/page/home/notLoggedIn/img-man-on-mountain.png';
+import Image from "next/image";
+import ContainerComponent from "@/chunk/containerComponent";
+import TitleComponent from "@/chunk/titleComponent";
+import ParagraphComponent from "@/chunk/paragraphComponent";
+
+// Creating and exporting fourth section of home page as default
+export default function FourthSectionComponent():ReactNode {
+    // Returning JSX
+    return (
+        <section className={'overflow-hidden'}>
+            <main className={'relative lg:h-[500px] h-[300px]'}>
+                <Image
+                    alt={'Image of a man standing on top of mountain'}
+                    src={mainImage.src}
+                    width={1024}
+                    height={500}
+                    className={'w-full h-full absolute top-0 left-0 blur-xl -z-10 scale-110'}
+                />
+                <ContainerComponent className={'lg:h-[500px] h-[300px] lg:flex lg:items-center lg:justify-center'}>
+                    <div>
+                        <TitleComponent color={'text-gray-500'} tier={1} className={'lg:text-center text-left'}>
+                            The Power of br
+                            Small Daily Wins
+                        </TitleComponent>
+                        <ParagraphComponent color={'text-white'} className={'lg:text-center text-left'}>
+                            Consistency is the silent architect of success, shaping our lives with each small, intentional action. In the pursuit of our goals, whether in health, productivity, or personal development, it is the unwavering commitment to daily rituals that propels us forward.
+                        </ParagraphComponent>
+                    </div>
+                </ContainerComponent>
+            </main>
+        </section>
+    );
+}
