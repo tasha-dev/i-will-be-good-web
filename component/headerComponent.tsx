1d463262f8d9c0861c00b2d0bed93b476c52a2fd
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-04 15:58:55 +0330

    changed shadow of header when scrolled

--- component/headerComponent.tsx
+++ component/headerComponent.tsx
@@ -23,7 +23,7 @@ export default function HeaderComponent({isUserLoggedIn}:propsType):ReactNode {
     return (
         <header
             data-scrolled={isScrolled}
-            className={'lg:fixed top-0 left-0 w-full z-10 transition-all duration-500 lg:data-[scrolled="true"]:bg-white lg:data-[scrolled="true"]:shadow-2xl lg:data-[scrolled="false"]:bg-transparent lg:data-[scrolled="false"]:shadow-none'}
+            className={'lg:fixed top-0 left-0 w-full z-10 transition-all duration-500 lg:shadow-xl lg:data-[scrolled="true"]:bg-white lg:data-[scrolled="true"]:shadow-black/5 lg:data-[scrolled="false"]:bg-transparent lg:data-[scrolled="false"]:shadow-transparent'}
         >
             <ContainerComponent noPaddingY className={
                 (isUserLoggedIn)
