d908428292c2922dd3b5697e4ac9747e3bbe5d5c
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-09 13:31:45 +0330

    added meditating dashboard page

---
+++ app/dashboard/meditation/page.tsx
@@ -0,0 +1,22 @@
+// Importing part
+import {ReactNode} from "react";
+import PageComponent from "@/component/pageComponent";
+import TitleComponent from "@/chunk/titleComponent";
+import CalendarComponent from "@/component/calendar/calendarComponent";
+import DrugInfoComponent from "@/component/drugInfoComponent";
+
+// Creating and exporting meditation report page as default
+export default function MeditationReportPage():ReactNode {
+    // Defining placeholder data to render
+    const data: string[] = ['2024-02-08T20:30:00.000Z', '2024-01-08T20:30:00.000Z'];
+
+    // Returning JSX
+    return (
+        <PageComponent loginRequired isDashboard>
+            <TitleComponent color={'text-themeBlue'} tier={2}>Meditation Report</TitleComponent>
+            <ul className={'flex flex-col gap-[20px]'}>
+                <CalendarComponent activeDays={data} />
+            </ul>
+        </PageComponent>
+    );
+}
