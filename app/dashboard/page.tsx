2d718e5d5e9fcff38caeadbf7abf1c927b8618de
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-09 11:39:42 +0330

    added rounting system of the dashboard home page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,15 @@
 // Codes by mahdi tasha
+// Forcing nextJS to render this component as client side component
+'use client';
+
 // Importing part
-import {ReactNode} from "react";
+import {useRouter} from "next/navigation";
 
 // Creating and exporting dashboard page as default
-export default function DashboardPage():ReactNode {
-    // TODO:Redirect to select page
-    // Returning JSX
-    return (
-        <div>
-            Hello
-        </div>
-    );
+export default function DashboardPage():void {
+    // Defining router to redirect the page
+    const router = useRouter();
+
+    // Redirecting to the select dashboard page when user comes to this page
+    router.push('/dashboard/select');
 }
