92e4e2571935f2fc1e45747020738bc3b3eb278e
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-19 17:27:27 +0330

    handled auth in home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,18 @@ import {ReactNode} from "react";
 import PageComponent from "@/component/pageComponent";
 import HomeNotLoggedIn from "@/component/innerPage/notLoggedIn/homeNotLoggedIn";
 import HomeLoggedIn from "@/component/innerPage/loggedIn/homeLoggedIn";
+import useFirebaseAuth from "@/hook/firebase/useFirebaseAuth";
 
 // Creating and exporting home page as default
 export default function HomePage():ReactNode {
+    // Defining firebase auth
+    const auth = useFirebaseAuth();
+
     // Returning JSX
     return (
         <PageComponent loginRequired={false}>
             {
-                (true)
+                (auth.user !== null)
                     ? <HomeLoggedIn />
                     : <HomeNotLoggedIn />
             }
