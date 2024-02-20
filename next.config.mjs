0fe9cc93ccd2b8a8defd05d9920aca2599cf2704
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-20 16:15:30 +0330

    added comments of next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// Defining type of configs of nextJS
 /** @type {import('next').NextConfig} */
+
+// Defining the configs
 const nextConfig = {};
 
+// Exporting configs as default
 export default nextConfig;
