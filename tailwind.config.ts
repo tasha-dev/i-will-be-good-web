31c61a737ee7727c2cea3a551217e6fe441ba5b2
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-20 16:14:31 +0330

    addde tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
+// Codes by mahdi tasha 
+// Importing type of tailwindCSS configss
 import type { Config } from "tailwindcss";
 
+// Defining configs of tailwind
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/*.{ts,tsx,js,jsx}',
+    './app/**/*.{ts,tsx,js,jsx}',
+    './chunk/*.{ts,tsx,js,jsx}',
+    './chunk/**/*.{ts,tsx,js,jsx}',
+    './component/*.{ts,tsx,js,jsx}',
+    './component/**/*.{ts,tsx,js,jsx}'
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
+
+// Exporting the configs
 export default config;
