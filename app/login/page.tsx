d04f053fbf64406856ecc4b4ac9813d76bb01522
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-21 17:41:31 +0330

    added some part of login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,13 +2,17 @@
 // Importing part
 import PageComponent from "@/component/pageComponent";
 import { ReactNode } from "react";
+import MainImage from '@/public/img/page/public/img-woman-meditating.png';
+import Image from "next/image";
+import FormComponent from "@/component/section/login/formComponent";
 
 // Creating and exporting login page as default
 export default function LoginPage():ReactNode {
   // Returning JSX
   return (
     <PageComponent>
-      sd
+       <Image alt="Image of a woman meditating" src={MainImage.src} width={1000} height={1000} className="h-[300px] w-full object-cover mb-[50px]" />
+       <FormComponent />
     </PageComponent>
   );
 }
---
+++ component/section/login/formComponent.tsx
@@ -0,0 +1,60 @@
+// Codes by mahdi tasha
+// Forcing nextJS to render this component as client side component
+'use client';
+
+// Importing part
+import { ReactNode } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from 'zod';
+import InputComponent from "@/chunk/inputComponent";
+
+// Defining type of form
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8).max(12)
+})
+
+type formType = z.infer<typeof formSchema>;
+
+// Creating and exporting form component as default
+export default function FormComponent():ReactNode {
+  // Defining state of component
+  // Defining useForm hook
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: {
+      errors,
+      isValidating
+    }
+  } = useForm<formType>({
+    resolver: zodResolver(formSchema)
+  })
+
+  // Defining a function to handle submit event
+  const onSubmitHandler:SubmitHandler<formType> = (data) => {
+    console.log(data);
+  }
+
+  // Returning JSX
+  return (
+    <form action="#" onSubmit={handleSubmit(onSubmitHandler)}>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-[20px] mb-[20px]">
+         <InputComponent registerName="email" register={register} label="Email" errorText={errors.email?.message} />
+         <InputComponent registerName="password" register={register} label="Password" errorText={errors.password?.message} />
+      </div> 
+      {
+        (errors.root?.message)
+          ? (
+            <div className="mt-[20px]">
+              <p className="text-red-600 lg:text-[16px] text-[13px] font-bold">
+                {errors.root?.message}
+              </p>
+            </div>
+          ) : false
+      }
+</form>
+  );
+}
