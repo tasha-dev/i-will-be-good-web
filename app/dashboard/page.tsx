33ffe5e022df1c5d1b3f56d7bdf4c7005e783a42
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-22 15:07:35 +0330

    added dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,13 +2,16 @@
 // Importing part
 import { ReactNode } from "react";
 import DashboardPageComponent from "@/component/dashboard/dashboardPageComponent";
+import TitleComponent from "@/chunk/titleComponent";
 
 // Creating and exporting dashboard page as default
 export default function DashboardPage():ReactNode {
   // Returning JSX
   return (
     <DashboardPageComponent>
-    asdsd
+      <div className="h-[calc(500px)] w-full border border-dashed border-themeBlue rounded-[20px] flex items-center justify-center">
+        <TitleComponent tier={1} theme="blue" className="text-center">Please select a subject from the left.</TitleComponent>
+      </div>
     </DashboardPageComponent>
   );
 }
